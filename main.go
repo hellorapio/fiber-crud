346bd2aeb78ee4a439a8807f87d69c6345f881b5
Anubis <anubis356@outlook.com>
2024-08-04 18:22:46 +0300

    feat: hello world endpoint

--- main.go
+++ main.go
@@ -1,11 +1,20 @@
-package fibercrud
+package main
 
 import (
 	"fmt"
+
+	"github.com/gofiber/fiber/v2"
 )
 
 func main() {
+	app := fiber.New()
 
 	fmt.Println("Simple crud application using fiber")
 
+	app.Get("/", func(c *fiber.Ctx) error {
+		return c.JSON(fiber.Map{"message": "Hello, World!"})
+		// return c.SendString("Hello, World!")
+	})
+
+	app.Listen(":3000")
 }
